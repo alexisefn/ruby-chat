322ecc67e167da7bc965e9e5574ab8ec36bc1039
4lerce <203114550+4lerce@users.noreply.github.com>
2025-03-13 17:07:15 -0300

    Update hola.rb
    Actualizado con el objetivo del día.

--- hola.rb
+++ hola.rb
@@ -7,36 +7,60 @@ def mostrar_mensaje
   puts "Ingresa algo y lo mostararé en pantalla"
   mensaje = gets.chomp
   
-  loop do  
-    puts "¿Cuantas veces quieres ver el mensaje?"
-    repeticiones = gets.chomp
+  opcion = ""
+
+  loop do
+    puts "¿Qué quieres hacer con este mensaje? Elige una opción:"
+    puts "1) Mostrar mensaje un número de veces"
+    puts "2) Mostrar mensaje al revés"
+
+    opcion = gets.chomp
+
+    break if opcion == '1' || opcion == '2'
+
+    puts "Lo siento, no ingresaste una opción válida. Intentalo nuevamente."
+  end
+  
+  if opcion == "1"
+    loop do  
+      puts "¿Cuantas veces quieres ver el mensaje?"
+      repeticiones = gets.chomp
     
-    begin
-      numero = Integer(repeticiones)
-      contador = 1
-      
-      loop do
-        puts mensaje
-        contador += 1
-        break if contador > numero.to_i
-      end
-      break
-    
-    rescue ArgumentError
-      puts "Lo siento, no ingresaste un número. Intentalo nuevamente."
+      begin
+        numero = Integer(repeticiones)
+        numero.times { puts mensaje }
+        break
+      rescue ArgumentError
+        puts "Lo siento, no ingresaste un número. Intentalo nuevamente."
     end
+  end
+  
+  elsif opcion == "2"
+    puts "#{mensaje.reverse}"
   end
 end
 
 loop do
   mostrar_mensaje
-  print "¿Quieres ingresar algo más? (Y/N): "
-  respuesta = gets.chomp.upcase
-  
+
+  respuesta = ""
+
+  loop do
+    print "¿Quieres ingresar algo más? (Y/N): "
+    respuesta = gets.chomp.upcase
+
+    break if respuesta == 'Y' || respuesta == 'N'
+
+    puts "Lo siento, no ingresaste una opción válida. Intentalo nuevamente."
+  end
+
   break if respuesta == 'N'
 end
 
 puts "¡Hasta la próxima!"
 
-# Notas
-# Validar Y/N
+# 13/03/2025
+# Objetivo del día
+# Agregar opciones para:
+# 1) Mostrar mensaje un número de veces determinado
+# 2) Mostrar mensaje al revés
