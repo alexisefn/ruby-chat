d0c489b6350828af4286aed8457b13c09031eef8
4lerce <203114550+4lerce@users.noreply.github.com>
2025-03-17 17:54:02 -0300

    update

--- persona/persona.rb
+++ persona/persona.rb
@@ -1,16 +1,62 @@
 require 'json'
 
-persona = {
-  nombre: "Alerce",
-  edad: 32,
-  profesion: "Desarrollador",
-  hobbies: ["música", "caminar", "programar"]
-}
+# Definir clase
+class Persona
+  attr_accessor :nombre, :edad, :profesion, :pasatiempo
 
-json_data = persona.to_json
+  def initialize(nombre, edad, profesion, pasatiempo)
+    @nombre = nombre
+    @edad = edad
+    @profesion = profesion
+    @pasatiempo = pasatiempo
+  end
 
-File.open("persona.json", "w") do |file|
-  file.write(json_data)
+  # Método para convertir datos en un hash (JSON)
+  def to_h
+    {
+      nombre: @nombre,
+      edad: @edad,
+      profesion: @profesion,
+      pasatiempo: @pasatiempo
+    }
+  end
 end
 
-puts "Datos guardados en persona.json"
+# Función para cargar datos desde archivo JSON
+def cargar_personas
+  if File.exist?('persona.json') && !File.zero?('persona.json')
+    file = File.read('persona.json')
+    JSON.parse(file, symbolize_names: true) rescue []
+  else
+    []
+  end
+end
+
+# Función para guardar datos en un archivo en un archivo JSON
+def guardar_personas(personas)
+  File.open('persona.json', 'w') do |file|
+    file.write(JSON.pretty_generate(personas))
+  end
+end
+
+# Ingresar nueva persona
+print "Ingresa tu nombre: "
+nombre = gets.chomp
+print "Ingresa tu edad: "
+edad = gets.chomp
+print "Ingrese profesión: "
+profesion = gets.chomp
+print "Ingresa un pasatiempo: "
+pasatiempo = gets.chomp
+
+# Crear persona y agregarla a la lista
+nueva_persona = Persona.new(nombre, edad, profesion, pasatiempo)
+personas = cargar_personas
+
+# Agregar persona al array
+personas << nueva_persona.to_h
+
+# Guardar persona
+guardar_personas(personas)
+
+puts "¡Persona registrada exitosamente!"
