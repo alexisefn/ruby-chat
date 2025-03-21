bd061aa8509347441b68ac81430dbb9b6f742416
4lerce <203114550+4lerce@users.noreply.github.com>
2025-03-21 15:54:12 -0300

    Actualización: Modificar agregado a CRUD.

--- crud/crud.rb
+++ crud/crud.rb
@@ -17,6 +17,7 @@ puts "¡Hola!"
 puts "¿Qué desea hacer?"
 puts "1. Ingresar nuevos datos"
 puts "2. Ver datos guardados"
+puts "3. Modificar datos"
 opcion = gets.chomp.to_i
 
 if opcion == 1
@@ -24,8 +25,12 @@ if opcion == 1
   nombre = gets.chomp
   puts "Ingrese su edad:"
   edad = gets.chomp.to_i
+  
+  # Obtener el último ID registrado para luego asignar uno nuevo
+  # Cada entrada debe tener su propio ID, de lo contrario es más difícil modificar o eliminar entradas
+  ultimo_id = datos.empty? ? 1 : datos.last["id"] + 1
 
-  datos << { "nombre" => nombre, "edad" => edad }
+  datos << { "id" => ultimo_id, "nombre" => nombre, "edad" => edad }
 
   File.open(archivo, "w") do |f|
     f.write(JSON.pretty_generate(datos))
@@ -35,11 +40,50 @@ if opcion == 1
 
 elsif opcion == 2
   if datos.empty?
-    puts "No hay datos guardados."
+    puts "Lo siento, no hay datos guardados."
   else
     puts "Lista de datos guardados:"
-    datos.each_with_index do |persona, i|
-      puts "#{i + 1}. Nombre: #{persona["nombre"]}, Edad: #{persona["edad"]}"
+    datos.each do |persona|
+      puts "ID: #{persona["id"]} - Nombre: #{persona["nombre"]}, Edad: #{persona["edad"]}"
+    end
+  end
+
+elsif opcion == 3
+  if datos.empty?
+    puts "Lo siento, no puede realizar esta acción porque no hay datos guardados."
+  else
+    puts "Lista de datos guardados:"
+    datos.each do |persona|
+      puts "ID: #{persona["id"]} - Nombre: #{persona["nombre"]}, Edad: #{persona["edad"]}"
+    end
+
+    puts "Ingrese el ID  de la persona que desea modificar:"
+    id_modificar = gets.chomp.to_i
+
+    # Busca el registro que coincida con el ID ingresado
+    persona = datos.find { |p| p["id"] == id_modificar }
+
+    if persona
+      puts "Ingrese los nuevos datos"
+      puts "Deje vacio y presione Enter para no modificar"
+      puts "Nombre: " # Sugerencia: Cambiar for print
+      nuevo_nombre = gets.chomp
+      puts "Edad: "
+      nueva_edad = gets.chomp
+
+      # Actualizar solo si el usaurio ingresó un nuevo valor
+      # De lo contrario se saltará el proceso
+      persona["nombre"] = nuevo_nombre unless nuevo_nombre.empty?
+      persona["edad"] = nueva_edad.to_i unless nueva_edad.empty?
+
+      File.open(archivo, "w") do |f|
+        f.write(JSON.pretty_generate(datos))
+      end
+
+      puts "¡Datos modificados con éxito!"
+
+    else
+      puts "ID no encontrado"
     end
   end
 else
